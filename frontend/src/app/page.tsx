c686240c7fe6c77b9f153db80e8855be76d0e2fa
JohnGuilding <jwguilding@gmail.com>
2023-05-13 10:27:21 +0100

    Add AA sdk code to create & submit userOp to bundler

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,43 +2,29 @@
 
 import NewTransactionButton from "./components/NewTransactionButton";
 import { ethers, Wallet } from "ethers";
-import { useEffect, useState } from "react"
-import { wrapProvider, ERC4337EthersProvider } from "@account-abstraction/sdk"
+import { useEffect, useState } from "react";
+import { wrapProvider, ERC4337EthersProvider } from "@account-abstraction/sdk";
 
-const privateKey = "0x8bc8492537b7a65d3698ada8ab1169964e3880f97c0139359a6f2d7095879bfd"
-const entryPointAddress = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789"
-const bundlerUrl = "http://localhost:3000/rpc"
-const rpcUrl = "http://localhost:8545"
+const privateKey =
+  "0x8bc8492537b7a65d3698ada8ab1169964e3880f97c0139359a6f2d7095879bfd";
+const entryPointAddress = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789";
+const bundlerUrl = "http://localhost:3000/rpc";
+const rpcUrl = "http://localhost:8545";
 
 export default function Home() {
-  const provider = new ethers.providers.JsonRpcProvider(
-    rpcUrl,
-    {
-      name: 'localhost',
-      chainId: 31337,
-    },
-  )
-  const signer = new Wallet(privateKey, provider)
-
-  const [aaProvider, setAaProvider] = useState<ERC4337EthersProvider | undefined>()
-  const [address, setAddress] = useState<string | undefined>()
+  const provider = new ethers.providers.JsonRpcProvider(rpcUrl, {
+    name: "localhost",
+    chainId: 31337,
+  });
+  const signer = new Wallet(privateKey, provider);
+  const [address, setAddress] = useState<string | undefined>();
 
   useEffect(() => {
-    const getAaProvider = async () => {
-      const tempAaProvider = await wrapProvider(
-        provider,
-        {
-          entryPointAddress,
-          bundlerUrl,
-        },
-        signer,
-      )
-
-      setAaProvider(tempAaProvider)
-      setAddress(await tempAaProvider.getSigner().getAddress())
-    }
-    getAaProvider()
-  }, [])
+    const getAddress = async () => {
+      setAddress(await signer.getAddress());
+    };
+    getAddress();
+  }, []);
 
   return (
     <main className="flex items-center justify-between p-24">
@@ -46,7 +32,7 @@ export default function Home() {
         <div>Multi-sig Balance: 1 ETH</div>
         <div>Address: {address}</div>
         <div>
-          <NewTransactionButton />
+          <NewTransactionButton provider={provider} signer={signer} />
         </div>
       </div>
       <div>
