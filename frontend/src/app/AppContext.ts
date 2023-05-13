66094e29751a681cc0673a46e0e27beb1f808374
Andrew Morris <voltrevo@gmail.com>
2023-05-13 16:03:25 +0100

    Sign payments

--- frontend/src/app/AppContext.ts
+++ frontend/src/app/AppContext.ts
@@ -1,7 +1,19 @@
 import { once } from '@s-libs/micro-dash';
 import { signer } from '@thehubbleproject/bls';
-import blsDomain from './blsDomain';
 import makeId from '@/utils/makeId';
+import * as io from 'io-ts';
+import blsDomain from './blsDomain';
+import { ethers } from 'ethers';
+import { solG1 } from '@thehubbleproject/bls/dist/mcl';
+
+const Payment = io.type({
+  token: io.string,
+  to: io.string,
+  amount: io.number,
+  description: io.string,
+});
+
+type Payment = io.TypeOf<typeof Payment>;
 
 export default class AppContext {
   static getSingleton = once(async () => {
@@ -19,4 +31,27 @@ export default class AppContext {
   });
 
   constructor(public signer: signer.BlsSignerInterface) {}
+
+  /**
+   * Aggregate these with:
+   *
+   * ```ts
+   * import { signer } from '@thehubbleproject/bls';
+   *
+   * const sig1 = ctx.signPayment(payment1);
+   * const sig2 = ctx.signPayment(payment2);
+   *
+   * const aggSig = signer.aggregate([sig1, sig2]);
+   * ```
+   */
+  signPayment(payment: Payment): solG1 {
+    const encodedPayment = AppContext.encodePayment(payment);
+
+    return this.signer.sign(ethers.utils.hexlify(encodedPayment));
+  }
+
+  private static encodePayment(payment: Payment) {
+    // TODO: Get the actual message that needs to be signed
+    return new TextEncoder().encode(JSON.stringify(payment));
+  }
 }
