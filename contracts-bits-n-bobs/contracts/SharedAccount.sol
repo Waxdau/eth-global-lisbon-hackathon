d30b8e413e83e7b8eefc684baa9e693de38d46b1
Andrew Morris <voltrevo@gmail.com>
2023-05-13 14:54:10 +0100

    Simplify spend

--- contracts-bits-n-bobs/contracts/SharedAccount.sol
+++ contracts-bits-n-bobs/contracts/SharedAccount.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "../account-abstraction/contracts/samples/bls/lib/BLSOpen.sol";
 
 struct Action {
     address to;
@@ -11,13 +10,11 @@ struct Action {
 }
 
 contract SharedAccount {
-    bytes32 BUNDLE_DOMAIN = bytes32(uint256(0xfeedbee5));
-
     address public owner;
     mapping(uint256 => uint256[4]) public members;
     uint256 public memberCount;
 
-    // Key is hash((memberCount, token))
+    // Key is hash((sigCount, token))
     mapping(bytes32 => uint256) public allowances;
 
     constructor() {
@@ -25,22 +22,15 @@ contract SharedAccount {
     }
 
     function spend(
+        uint8 signersBitField,
         IERC20 token,
         address to,
-        uint256 amount,
-        uint256 signingMembers,
-        uint256[2] memory signature
+        uint256 amount
     ) public {
-        Action memory action = Action({
-            to: to,
-            value: 0,
-            data: abi.encodeCall(token.transfer, (to, amount))
-        });
-
-        uint256 signingMemberCount = countSetBits(signingMembers);
+        uint256 sigCount = countBitField(signersBitField);
 
         bytes32 allowanceKey = keccak256(
-            abi.encode(memberCount, address(token))
+            abi.encode(sigCount, address(token))
         );
 
         uint256 allowance = allowances[allowanceKey];
@@ -49,52 +39,10 @@ contract SharedAccount {
             revert("Amount exceeds allowance");
         }
 
-        uint256[4][] memory publicKeys = new uint256[4][](
-            signingMemberCount
-        );
-
-        uint256 i = 0;
-
-        for (uint256 j = 1; j < memberCount; j++) {
-            if (signingMembers & (1 << j) != 0) {
-                publicKeys[i] = members[j];
-                i++;
-            }
-        }
-
-        bool verified = verify(publicKeys, abi.encode(action), signature);
-
-        if (!verified) {
-            revert("Not verified");
-        }
-
         token.transfer(to, amount);
     }
 
-    function verify(
-        uint256[4][] memory publicKeys,
-        bytes memory message,
-        uint256[2] memory signature
-    ) internal returns (bool) {
-        uint256[2] memory messagePoint = BLSOpen.hashToPoint(
-            BUNDLE_DOMAIN,
-            message
-        );
-
-        uint256[2][] memory messagePoints = new uint256[2][](publicKeys.length);
-
-        for (uint256 i = 0; i < publicKeys.length; i++) {
-            messagePoints[i] = messagePoint;
-        }
-
-        return BLSOpen.verifyMultiple(
-            signature,
-            publicKeys,
-            messagePoints
-        );
-    }
-
-    function countSetBits(uint256 x) public pure returns (uint256) {
+    function countBitField(uint8 x) public pure returns (uint256) {
         uint256 count;
 
         while (x != 0) {
