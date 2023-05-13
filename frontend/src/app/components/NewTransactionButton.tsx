52bcf567059618365d724d9cb5352e7d99f3e0e7
JohnGuilding <jwguilding@gmail.com>
2023-05-13 14:05:23 +0100

    Log recipient balance

--- frontend/src/app/components/NewTransactionButton.tsx
+++ frontend/src/app/components/NewTransactionButton.tsx
@@ -6,7 +6,7 @@ interface NewTransactionButtonProps {
   provider: ERC4337EthersProvider | undefined;
 }
 
-const recipient = "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC"; // Hardhat Account 3/4?
+const recipient = "0x90F79bf6EB2c4f870365E785982E1f101E93b906";
 
 const NewTransactionButton = ({ provider }: NewTransactionButtonProps) => {
   const newTransaction = async () => {
@@ -17,10 +17,13 @@ const NewTransactionButton = ({ provider }: NewTransactionButtonProps) => {
     const transaction = await signer.sendTransaction({
       to: recipient,
       value: ethers.utils.parseEther("0.5"),
-      data: '0x'
+      data: "0x",
     });
     const reciept = await transaction.wait();
     console.log("reciept", reciept);
+
+    const recieptBalance = await provider.getBalance(recipient);
+    console.log("recieptBalance", ethers.utils.formatEther(recieptBalance));
   };
 
   return (
