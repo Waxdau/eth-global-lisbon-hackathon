d1b157f705508c244cb0a29dd3bade4acf795275
JohnGuilding <jwguilding@gmail.com>
2023-05-14 08:09:42 +0100

    Fix createWallet function

--- frontend/src/createWallet.ts
+++ frontend/src/createWallet.ts
@@ -1,13 +1,29 @@
 import { solG2 } from '@thehubbleproject/bls/dist/mcl';
 import AppContext from './AppContext';
 import { AccountAPI } from './account/AccountAPI';
+import { BLSGroupVerifier__factory } from 'account-abstraction';
+import { ethers } from 'ethers';
 
 export default async function createWallet(
   ctx: AppContext,
-  _publicKeys: solG2[],
+  publicKeys: solG2[],
 ) {
   const accountApi = ctx.aaProvider?.smartAccountAPI as AccountAPI;
-  await accountApi.eip4337Manager.blsVerifier.setupGroup(_publicKeys);
+  const provider = ctx.aaProvider;
+
+  const fundAccountTransaction = await ctx.hhSigner.sendTransaction({
+    to: await provider.getSigner().getAddress(),
+    value: ethers.utils.parseEther('10'),
+    data: '0x',
+  });
+  await fundAccountTransaction.wait();
+
+  const blsVerifier = BLSGroupVerifier__factory.connect(
+    await accountApi.eip4337Manager.blsVerifier(),
+    provider.getSigner(),
+  );
+
+  await blsVerifier.setupGroup(publicKeys);
 
   await accountApi.getNonce();
   return accountApi.accountContract?.address;
